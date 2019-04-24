3f9189d5d25e94cc819d3b1440fe2c3c1d0f9013
nikol <nikol@DESKTOP-IK0KQ1U>
2019-04-24 22:54:25 +0200

    Server class created and added basic functionalities

---
+++ ServerCalculator/src/main/Server.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	
+	static Socket socketForCommunication = null;
+	static ServerSocket serverSocket = null;
+	
+	public static void main(String[] args) {
+		
+		try {
+			serverSocket = new ServerSocket(9000);
+			
+			while(true) {
+				System.out.println("Waiting for a connection...");
+				socketForCommunication = serverSocket.accept();
+				System.out.println("Connection established.");
+			}
+		} catch (IOException e) {
+			System.out.println("Error w");
+		}
+	}
+}
